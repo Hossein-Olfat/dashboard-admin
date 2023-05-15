d0d80b4f4db4b098bcf7561e96391a079ab7ea0d
Hossein-Olfat <h1382olfat@gmail.com>
2023-05-15 11:09:42 -0700

    init project

---
+++ my-project/src/App.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return <div>App</div>;
+}
---
+++ my-project/src/routes.js
@@ -0,0 +1,6 @@
+// const routes = [
+//   { path: "/", element: <Home /> },
+//   { path: "/users" },
+//   { path: "/userList" },
+// ];
+// export { routes };
